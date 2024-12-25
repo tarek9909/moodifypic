cdc1a69b6217df6d4ee7c7e5369f824d5b7af64d
tarek9909 <114114286+tarek9909@users.noreply.github.com>
2024-12-25 20:23:23 +0200

    Update imageur.py

--- imageur.py
+++ imageur.py
@@ -4,6 +4,7 @@ import uuid
 import time
 from werkzeug.utils import secure_filename
 from flask_cors import CORS  # Import CORS
+from threading import Thread  # Import Thread
 
 app = Flask(__name__)
 
